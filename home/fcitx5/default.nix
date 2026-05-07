5e8b4b7b743a7cf4c59ac97c692018388ca13978
Silas <silaszk@126.com>
2026-05-07 22:03:23 +0800

    config fcitx5 and install oh-my-rime

--- home/fcitx5/default.nix
+++ home/fcitx5/default.nix
@@ -1,7 +1,22 @@
 { config, pkgs, ... }:
 
 {
-
+  programs.fcitx5 = {
+    enable = true;
+    rime = {
+      enable = true;
+      customSchemes = [
+        {
+          name = "oh-my-rime";
+          src = pkgs.fetchgit {
+            url = "https://github.com/Mintimate/oh-my-rime.git";
+            rev = "main";
+            sha256 = lib.fakeSha256;
+          };
+        }
+      ];
+    };
+  };
 
 
 }
